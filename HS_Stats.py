f677cc8c1fb683f5c0262f21b72b0e4a627179b5
OrGindes <gindesor@gmail.com>
2020-03-25 20:48:25 +0200

    Code review for CLI branch
    Signed-off-by: OrGindes <gindesor@gmail.com>

--- HS_Stats.py
+++ HS_Stats.py
@@ -1,31 +1,33 @@
 """
 ITC Data-Mining Project
-By: Or Gindes, Dor Sklar
+By: Or Gindes, Dor Sklar, Marria Padalko
 """
 
 from feed_parser import feed_parser
 from game_parser import game_parser
 from selenium.common.exceptions import WebDriverException
-
 from argparse_cli import check_args
-
-INFINITE = check_args()[0]  # Set to True for indefinite value collection
-N_ITERATIONS = check_args()[1]  # Only relevant when INFINITE is set to False - determine number of scraping iterations
-QUIET = check_args()[2]  # if not provided - defaults to False. When set to True - suppress driver window popup
 
 
 def main():
     """This function can be set to gather HearthStone data indefinitely (until key prompt)
      or for a set number of iterations"""
+    arguments = check_args()
+    # infinite - Set to True for indefinite value collection
+    infinite = arguments[0]
+    # number_of_iterations - Only relevant when INFINITE is set to False - determine number of scraping iterations
+    number_of_iterations = arguments[1]
+    # quiet - if not provided defaults to False. When set to True - suppress driver window popup
+    quiet = arguments[2]
     i = 0
-    while (i < N_ITERATIONS) or INFINITE:
+    while (i < number_of_iterations) or infinite:
         i += 1
         matches = feed_parser()
         for match in matches:
             try:
                 print(match)
                 match_url, winner, loser = match[0], match[1], match[2]
-                winner_deck, loser_deck = game_parser(match_url, winner, loser, QUIET)
+                winner_deck, loser_deck = game_parser(match_url, winner, loser, quiet)
                 print("The Winning Deck of the match is:")
                 print(winner_deck)
                 print("The Losing Deck of the match is:")
--- argparse_cli.py
+++ argparse_cli.py
@@ -1,50 +1,51 @@
+"""
+ITC Data-Mining Project
+By: Or Gindes, Dor Sklar, Marria Padalko
+
+This function accept argument from user regarding operational parameters
+"""
+
+import sys
 import argparse
-import math
+
+MIN_NUM_OF_ARG = 2
 
 
 def parse_args_cli():
-    """Parsing args from cli input"""
+    """
+    Parsing args from cli input
+    :returns
+    infinity parameter - boolean, when set to True ill collect data until interrupt
+    quiet parameter - boolean, when set to True will suppress driver window popup
+    number of readings parameter - int, when not run in infinity mode, determine number of iterations
+    """
     parser = argparse.ArgumentParser(description="Parse 3 parameters")
-    parser.add_argument('-i', '--infinity', action='store_true', default=False, \
-                        help='Set to True for indefinite value collection')
-    parser.add_argument('-q', '--quiet', action='store_true', help='Quiet mode. \
-                        When set to True - suppress driver window popup', default=False)
-    parser.add_argument('--number_of_readings', '-n', type=int, default=1, help='Only relevant when INFINITE is set \
-                        to False - determine number of scraping iterations')
+    parser.add_argument('-i', '--infinity', action='store_true', default=False, help='Set to True for indefinite '
+                                                                                     'match collection')
+    parser.add_argument('-n', '--number_of_iterations', type=int, default=0, help='When not run with the infinity '
+                                                                                  'parameter - determine number of '
+                                                                                  'scraping iterations')
+    parser.add_argument('-q', '--quiet', action='store_true', default=False, help='Quiet mode. When set to True - '
+                                                                                  'suppress driver window popup')
     args = parser.parse_args()
-    return [args.infinity, args.number_of_readings, args.quiet]
+    if len(sys.argv) < MIN_NUM_OF_ARG:  # TODO: The function ran without any input, this shouldn't happen
+        parser.error("too few arguments")
+    return [args.infinity, args.number_of_iterations, args.quiet]
 
 
+# TODO: in Hs_Stats you are calling this function 3 times. you need to call it once and than parse the return not
+#  call it three times and take one argument from each function call (see how I did in HS_Stats now)
 def check_args():
-    """Checking infinity, etc"""
-    triple = parse_args_cli()
-    if triple[0]:
-        if triple[1]:
-            print('Either infinity either number, not both :)\n-i -n <NUMBER> yelds invalid input. Choose only 1 of them')
-            raise ValueError
-        triple[1] = math.inf
-    return triple
+    """Checking infinity, etc"""  # TODO: Need a better explanation of what the function does and returns (see above)
+    arguments = parse_args_cli()
+    if arguments[0]:
+        if arguments[1]:
+            print('Invalid input provided!\nPlease choose operation mode: Infinity mode (-i) for indefinite data '
+                  'collection or specify desired number of iterations (-n <NUMBER>)')
+            raise ValueError  # TODO: Handle this error in try/except, its incorrect to raise the error and ignore it
+        # arguments[1] = math.inf  # TODO: Why? the infinity argument is True or False, this isn't needed
+    return arguments
 
 
 if __name__ == '__main__':
-     check_args()
-
-
-# Click : unsuccessful
-# import click  # 7.1.1 version
-# @click.command()
-# @click.option('--infinity', '-i', is_flag=True,
-#               help='Do you want parse till infinity?:)')
-# @click.option('--number_of_feed_readings', '-n', default=1, prompt=True, type=int,
-#               help='Number?:)')
-# @click.option('--quiet', '-q', is_flag=True,
-#               help='Do you want quiet mode?')
-# def clickcli(infinity, number_of_feed_readings, quiet):
-#     """Processing arguments from cli"""
-#     click.echo('Inf: {}, num: {} and quiet: {}'.format(infinity, number_of_feed_readings, quiet))
-#     inf, num, qui = infinity, number_of_feed_readings, quiet
-#     return [inf, num, qui]
-#
-#
-# if __name__ == "__main__":
-#     clickcli()
+    check_args()
