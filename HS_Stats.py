d36c46ad656a32c96b885b8514768326a1ada48c
DorSklar <dorsklar@gmail.com>
2020-03-25 23:12:15 +0200

    Changes: * Updated feed_parser.py - now feed_parser() directly uses feed_matches() * Updated HS_Stats.py accordingly.
    Signed-off-by: DorSklar <dorsklar@gmail.com>

--- HS_Stats.py
+++ HS_Stats.py
@@ -2,11 +2,12 @@
 ITC Data-Mining Project
 By: Or Gindes, Dor Sklar, Mariia Padalko
 
-Hs_Stats.py is the main scrapping script which can be run for a number of iterations
-or indefinitely (until keyboard interrupt). Each iteration will scrape about 10 HearthStone matches
+Hs_Stats.py is the main scrapping script which can be run for a number of iterations or indefinitely
+(until keyboard interrupt).
+Each iteration will scrape up to 11 HearthStone matches
 """
 
-from feed_parser import feed_parser, feed_matches
+from feed_parser import feed_parser
 from game_parser import game_parser
 from selenium.common.exceptions import WebDriverException, NoSuchWindowException
 from urllib3.exceptions import MaxRetryError
@@ -23,9 +24,7 @@ def main():
     while (i < N_ITERATIONS) or INFINITE:
         i += 1
         try:
-            results = feed_parser()
-            matches = feed_matches(results['game_links'], results['player_ranks'], results['player_left_decks'],
-                                   results['player_right_decks'], results['left_players_win_lose'])
+            matches = feed_parser()
             for match in matches:
                 print(match)
                 match_url, winner, loser = match[0], match[1], match[2]
